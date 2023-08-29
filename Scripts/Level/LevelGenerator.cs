bdc0522c6eba6449f947828c5a3c0c6e426f4f3f
DarshanDEV1 <darshansahoo20@gmail.com>
2023-08-29 07:56:54 +0530

    Initial commit, Starting creating a game something similar to donkey kong game. I have created the assets and now I am creating the scriptable objects, to generate the level randomly.

---
+++ Scripts/Level/LevelGenerator.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Level", menuName = "Level Generator")]
+public class LevelGenerator : ScriptableObject
+{
+    //This is a scriptable object that is used to generator a level;
+}
